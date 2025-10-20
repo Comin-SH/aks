e03eb46c1c942b3f23f7d30ed54faadfe59c6813
alex-comin <a.flindt@comin.de>
2025-10-20 14:51:34 +0200

    update ArgoCD application configuration; change targetRevision to 9.0.2 and update server URL add kubectl provider with version constraints and update Helm provider configurations refactor argocd.tf to use kubectl_manifest for apps-of-apps and add dependency on helm_release update main.tf to include commented-out time_sleep resource for API readiness update providers.tf to specify versions for providers and replace kubernetes with kubectl provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,27 +1,37 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = ">=1.8.0"
 
+
+# Warum ~> x.0? Du bekommst automatisch Patches & Minor-Fixes, aber keine Major-Sprünge (Breaking Changes).
   required_providers {
     azapi = {
       source  = "azure/azapi"
+      version = "~> 2.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-    }
-    random = {
-      source  = "hashicorp/random"
+       version = "~> 4.0"
     }
     time = {
       source  = "hashicorp/time"
+      version = "~> 0.13"
     }
     azuread = {
       source  = "hashicorp/azuread"
+      version = "~> 3.0"
     }
     helm = {
       source  = "hashicorp/helm"
+      version = "~> 3.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    # Es wurde ein alternativer kubectl Provider von Gavin Bunney gewählt, da dieser die Erstellung des Clusters und Kubernetes Ressourcen mit einem Apply ermöglicht.
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.0"
     }
   }
 }
@@ -34,18 +44,19 @@ provider "azurerm" {
 provider "azuread" {}
 
 
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.k8s.kube_admin_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].cluster_ca_certificate)
+provider "kubectl" {
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
+  # load_config_file       = false
 }
 
 provider "helm" {
   kubernetes = {
-    host                   = azurerm_kubernetes_cluster.k8s.kube_admin_config[0].host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_admin_config[0].cluster_ca_certificate)
+    host                   = data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_admin_config[0].cluster_ca_certificate)
   }
 }
