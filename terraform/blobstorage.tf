d3eeff534716c966ed1972143ae60e553118e105
comin-alex <a.flindt@comin.de>
2025-10-22 10:44:53 +0200

    fix(terraform): Correct server URL in ArgoCD application and update resource group references in blob storage and key vault configurations

--- terraform/blobstorage.tf
+++ terraform/blobstorage.tf
@@ -2,12 +2,12 @@
 resource "azurerm_user_assigned_identity" "workload_identity" {
   name                = var.workload_identity_name
   location            = var.STORAGE_ACCOUNT_LOCATION
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_storage_account" "loki" {
   name                     = var.STORAGE_ACCOUNT_NAME
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.STORAGE_ACCOUNT_LOCATION
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -16,7 +16,7 @@ resource "azurerm_storage_account" "loki" {
 
 # Rolle "Storage Blob Data Contributor" auf Storage-Account-Ebene zuweisen
 resource "azurerm_role_assignment" "wi_blob_contrib" {
-  scope                = data.azurerm_storage_account.sa.id
+  scope                = azurerm_storage_account.loki.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.workload_identity.principal_id
   }
