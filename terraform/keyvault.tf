5fd5c5291d57a704434eeeaf6d597d488108e2bf
alex-comin <a.flindt@comin.de>
2025-10-24 13:23:15 +0200

    fix(terraform): Disable Grafana ingress for testing and update workload identity name variable

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -42,7 +42,7 @@ resource "azurerm_key_vault_secret" "grafana-admin-password" {
 
 # User Assigned Identity für Grafana (Workload Identity)
 resource "azurerm_user_assigned_identity" "monitoring" {
-  name                = "monitoring-identity"
+  name                = var.workload_identity_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
